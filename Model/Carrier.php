238f8d6549e22415af2304ab7b18b6bb7c4e3d20
Thomas Athanasiou <55364641+Thomas-Athanasiou@users.noreply.github.com>
2022-03-14 16:52:22 +0200

    docblocks and strict types update

--- Model/Carrier.php
+++ Model/Carrier.php
@@ -1,9 +1,11 @@
 <?php
     /**
      * Copyright © Magento, Inc. All rights reserved.
-     * See COPYING.txt for license details.
      *
-     * @author Thomas Athanasiou at Hippiemonkeys | @Thomas-Athanasiou
+     * @Thomas-Athanasiou
+     *
+     * @author Thomas Athanasiou at Hippiemonkeys
+     * @link https://github.com/Thomas-Athanasiou
      * @copyright Copyright (c) 2022 Hippiemonkeys (https://hippiemonkeys.com)
      * @package Hippiemonkeys_Dhl
      */
--- Model/Config/Source/PaymentType.php
+++ Model/Config/Source/PaymentType.php
@@ -1,9 +1,14 @@
 <?php
     /**
-     * @author Thomas Athanasiou at Hippiemonkeys | @Thomas-Athanasiou
+     * @Thomas-Athanasiou
+     *
+     * @author Thomas Athanasiou at Hippiemonkeys
+     * @link https://github.com/Thomas-Athanasiou
      * @copyright Copyright (c) 2022 Hippiemonkeys (https://hippiemonkeys.com)
      * @package Hippiemonkeys_Dhl
      */
+
+    declare(strict_types=1);
 
     namespace Hippiemonkeys\Dhl\Model\Config\Source;
 
